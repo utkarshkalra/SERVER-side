bba790a2380592b3d9a26dad68c92a56564142fa
utkarshkalra <utkarshkalra.2001@gmail.com>
2021-02-02 05:11:55 +0530

    adding p5 to call videoCapture

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Datastore=require('nedb');
 app.use(cors());
 
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb' }));
+app.use(express.json({limit: '5mb' }));
 
 // app.get('/',(req,res)=> app.render.static('public')
 
